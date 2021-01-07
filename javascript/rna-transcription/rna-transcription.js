a5682361ea001600104329e50a3238d5bdeabc15
czarjulius <juliusczar.jc@gmail.com>
2021-01-07 16:58:34 +0100

    rna-transcription challenge

---
+++ javascript/rna-transcription/rna-transcription.js
@@ -0,0 +1,23 @@
+//
+// This is only a SKELETON file for the 'RNA Transcription' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+const DNA = {
+G :'C',
+C : 'G',
+T :'A',
+A: 'U'
+}
+
+export const toRna = (dna) => {
+  if (dna) {
+    let RNA = ''
+    for(var char of dna){
+      RNA += DNA[char];
+    }
+    return RNA
+  } else {
+    return ''
+  }
+};
