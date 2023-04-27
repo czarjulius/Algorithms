960b814b16f322d7c9bd40a00ea725336be123ed
czarjulius <juliusczar.jc@gmail.com>
2023-04-27 11:08:29 +0100

    linked-list

---
+++ Udemy/linked-list/reverse-linked-list.js
@@ -0,0 +1,11 @@
+const reverseLinkedList = (head) => {
+  let prev = null;
+  let current = head;
+  while (current) {
+    let next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+  return prev;
+};
