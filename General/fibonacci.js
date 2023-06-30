18c91e092f7a5052e0df42e25f04d29c3764ba20
czarjulius <juliusczar.jc@gmail.com>
2023-06-30 17:29:31 +0100

    nFibonacci1

--- General/fibonacci.js
+++ General/fibonacci.js
@@ -1,3 +1,10 @@
+function nFibonacci1(num) {
+  if (num == 1) return 0;
+  if (num == 2) return 1;
+
+  return nFibonacci1(num - 1) + nFibonacci1(num - 2);
+}
+
 function nFibonacci(n, memo = {}) {
   if (n in memo) return memo[n];
   if (n <= 1) return n;
