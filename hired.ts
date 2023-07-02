69b4e4a448867a89198c0e73a7745e904a3021c2
czarjulius <juliusczar.jc@gmail.com>
2023-07-02 20:48:33 +0100

    refactor

--- hired.ts
+++ hired.ts
@@ -1,4 +1,6 @@
-// you are at the bottom of a staircase with n stairs. You can jump 1,2, or 3 stairs at a time. How many different ways can you jump up the stairs. write a javascript function for the case above
+// you are at the bottom of a staircase with n stairs. You can jump 1,2, or 3 stairs at a time.
+//How many different ways can you jump up the stairs.
+//write a javascript function for the case above
 
 function numWaysStairs(n) {
   if (n <= 2) {
