4ec1d74af44f09f78af066f8526afdf4f054863e
czarjulius <juliusczar.jc@gmail.com>
2023-08-01 15:41:35 +0100

    remove logo9

--- test/pagination.js
+++ test/pagination.js
@@ -1,64 +1,64 @@
-import React, { useEffect, useState } from "react";
-import "./styles.css";
+// import React, { useEffect, useState } from "react";
+// import "./styles.css";
 
-export default function App() {
-  const limit = 10;
-  const [name, setName] = useState("");
-  const [page, setPage] = useState(1);
-  const [pokemons, setPokemons] = useState([]);
+// export default function App() {
+//   const limit = 10;
+//   const [name, setName] = useState("");
+//   const [page, setPage] = useState(1);
+//   const [pokemons, setPokemons] = useState([]);
 
-  async function getPokemons(offset = 0) {
-    const response = await fetch(
-      `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
-    );
-    const data = await response.json();
-    console.log("called");
+//   async function getPokemons(offset = 0) {
+//     const response = await fetch(
+//       `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+//     );
+//     const data = await response.json();
+//     console.log("called");
 
-    return data.results;
-  }
+//     return data.results;
+//   }
 
-  useEffect(() => {
-    const nextDataOffset = (page-1) * limit
-    getPokemons(nextDataOffset).then((results) => setPokemons(results));
-  }, [page]);
+//   useEffect(() => {
+//     const nextDataOffset = (page-1) * limit
+//     getPokemons(nextDataOffset).then((results) => setPokemons(results));
+//   }, [page]);
 
-  function gotoPage(p) {
-    setPage(p);
-  }
+//   function gotoPage(p) {
+//     setPage(p);
+//   }
 
-  function filterPokemon() {
-    const newData = pokemons.filter(pokemon => pokemon.name.includes(name))
-    setPokemons(newData)
-    // TODO
-  }
+//   function filterPokemon() {
+//     const newData = pokemons.filter(pokemon => pokemon.name.includes(name))
+//     setPokemons(newData)
+//     // TODO
+//   }
 
-  return (
-    <div className="App">
-      <h1>Pokemon</h1>
-      <h2>Page: {page}</h2>
-      <input
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <button onClick={filterPokemon}>Filter</button>
+//   return (
+//     <div className="App">
+//       <h1>Pokemon</h1>
+//       <h2>Page: {page}</h2>
+//       <input
+//         type="text"
+//         value={name}
+//         onChange={(e) => setName(e.target.value)}
+//       />
+//       <button onClick={filterPokemon}>Filter</button>
 
-      <br />
-      <br />
+//       <br />
+//       <br />
 
-      {page > 1 ? (
-        <button onClick={()=> gotoPage(page - 1)}>Last Page</button>
-      ) : null}
-      <button onClick={() => gotoPage(page + 1)}>Next Page</button>
+//       {page > 1 ? (
+//         <button onClick={()=> gotoPage(page - 1)}>Last Page</button>
+//       ) : null}
+//       <button onClick={() => gotoPage(page + 1)}>Next Page</button>
 
-      <br />
-      <br />
+//       <br />
+//       <br />
 
-      {pokemons.map((pokemon) => (
-        <a key={pokemon.name} style={{ display: "block" }} href={pokemon.url}>
-          {pokemon.name}
-        </a>
-      ))}
-    </div>
-  );
-}
+//       {pokemons.map((pokemon) => (
+//         <a key={pokemon.name} style={{ display: "block" }} href={pokemon.url}>
+//           {pokemon.name}
+//         </a>
+//       ))}
+//     </div>
+//   );
+// }
