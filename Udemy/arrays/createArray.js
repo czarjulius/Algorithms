27a835cfe4a13a463dd03da7555eb2f60d17ef61
czarjulius <juliusczar.jc@gmail.com>
2023-08-01 15:41:03 +0100

    remove logo8

--- Udemy/arrays/createArray.js
+++ Udemy/arrays/createArray.js
@@ -1,47 +1,47 @@
-// class MyArray {
-//   constructor() {
-//     this.length = 0;
-//     this.data = {};
-//   }
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
 
-//   get(index) {
-//     const item = this.data[index];
-//     return item;
-//   }
+  get(index) {
+    const item = this.data[index];
+    return item;
+  }
 
-//   push(item) {
-//     this.data[this.length] = item;
-//     this.length++;
-//     return this.length;
-//   }
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
 
-//   pop() {
-//     const lastItem = this.data[this.length - 1];
-//     delete this.data[this.length - 1];
-//     this.length--;
-//     return lastItem;
-//   }
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
 
-//   delete(index) {
-//     const item = this.data[index];
-//     this.shiftItem(index);
-//   }
+  delete(index) {
+    const item = this.data[index];
+    this.shiftItem(index);
+  }
 
-//   shiftItem(index) {
-//     for (let i = index; i < this.length - 1; i++) {
-//       this.data[i] = this.data[i + 1];
-//     }
-//     delete this.data[this.length - 1];
-//     this.length--;
-//   }
-// }
+  shiftItem(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+}
 
-// const newArray = new MyArray();
-// newArray.push('julius');
-// newArray.push('good');
-// newArray.push('man');
-// newArray.push('cool');
-// newArray.get(3);
-// // newArray.pop()
-// newArray.delete(2);
-// console.log(newArray);
+const newArray = new MyArray();
+newArray.push('julius');
+newArray.push('good');
+newArray.push('man');
+newArray.push('cool');
+newArray.get(3);
+// newArray.pop()
+newArray.delete(2);
+console.log(newArray);
