6faf9e76fe3fef7d64e03d84283cb8d7ab21cfc9
czarjulius <juliusczar.jc@gmail.com>
2023-08-29 00:15:19 +0100

    tuesday commits12

--- hired.ts
+++ hired.ts
@@ -1,17 +1,17 @@
-// // you are at the bottom of a staircase with n stairs. You can jump 1,2, or 3 stairs at a time.
-// //How many different ways can you jump up the stairs.
-// //write a javascript function for the case above
+// you are at the bottom of a staircase with n stairs. You can jump 1,2, or 3 stairs at a time.
+//How many different ways can you jump up the stairs.
+//write a javascript function for the case above
 
-// function numWaysStairs(n) {
-//   if (n <= 2) {
-//     return n;
-//   }
+function numWaysStairs(n) {
+  if (n <= 2) {
+    return n;
+  }
 
-//   const ways = [1, 1, 2];
+  const ways = [1, 1, 2];
 
-//   for (let i = 3; i <= n; i++) {
-//     ways[i] = ways[i - 1] + ways[i - 2] + ways[i - 3];
-//   }
+  for (let i = 3; i <= n; i++) {
+    ways[i] = ways[i - 1] + ways[i - 2] + ways[i - 3];
+  }
 
-//   return ways[n];
-// }
+  return ways[n];
+}
