06a9c45f6e8bd175187acbca7f21c0ebf46dc31e
czarjulius <juliusczar.jc@gmail.com>
2023-09-14 12:07:05 +0100

    advanced nfibonnachi

--- General/fibonacci.js
+++ General/fibonacci.js
@@ -12,3 +12,14 @@ function nFibonacci(n, memo = {}) {
 }
 
 // nFibonacci(100);
+
+//list out all values to the nth term
+const nfibonnachi = (n) => {
+  let output = [0, 1];
+  for (let i = 2; i < n; i++) {
+    const nextValue = output[i - 1] + output[i - 2];
+    output.push(nextValue);
+  }
+
+  return output;
+};
