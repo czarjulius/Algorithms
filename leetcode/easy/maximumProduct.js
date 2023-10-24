0ae1f9fa3550e9b5a06e9690b25bcb222e4b6b1f
czarjulius <juliusczar.jc@gmail.com>
2023-10-24 23:18:53 +0100

    add extra case

--- leetcode/easy/maximumProduct.js
+++ leetcode/easy/maximumProduct.js
@@ -1,13 +1,13 @@
-var maximumProduct = function (nums) {
-  nums.sort((a, b) => a - b);
+// var maximumProduct = function (nums) {
+//   nums.sort((a, b) => a - b);
 
-  const n = nums.length;
-  if (nums.length < 3) return false;
+//   const n = nums.length;
+//   if (nums.length < 3) return false;
 
-  const product1 = nums[0] * nums[1] * nums[n - 1];
-  const product2 = nums[n - 3] * nums[n - 2] * nums[n - 1];
+//   const product1 = nums[0] * nums[1] * nums[n - 1];
+//   const product2 = nums[n - 3] * nums[n - 2] * nums[n - 1];
 
-  return Math.max(product1, product2);
-};
+//   return Math.max(product1, product2);
+// };
 
 // maximumProduct([3,4])
