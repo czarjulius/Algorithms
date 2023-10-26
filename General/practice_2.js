24dee745325b648e87c1b0fbe69880082105785a
czarjulius <juliusczar.jc@gmail.com>
2023-10-26 17:37:33 +0100

    get,set

--- General/practice_2.js
+++ General/practice_2.js
@@ -4,7 +4,6 @@ class Node {
     this.next = null;
   }
 }
-
 class SinglyLinkedList {
   constructor() {
     this.head = null;
@@ -31,11 +30,72 @@ class SinglyLinkedList {
       current = current.next;
     }
   }
-  pop() {}
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+      this.head = null;
+    }
+    return current;
+  }
+  shift() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    this.head = current.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return current;
+  }
+  unShift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let count = 0;
+    let current = this.head;
+    while (count !== index) {
+      current = current.next;
+      count++;
+    }
+    return current;
+  }
+  set(index, val) {
+    let foundNode = this.get(index);
+    if (foundNode) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
 }
-
-const list1 = new SinglyLinkedList();
-// list1.push('Julius')
-// list1.push('is')
-// list1.push('here')
-// list1.traverse()
+let list = new SinglyLinkedList();
+list.push('Julius');
+list.push('is');
+list.push('here');
+// list.push('again')
+// list.push('again')
+// list.traverse()
+list.get(0);
+list.set(0, 'oo');
+list.traverse();
+// list.get(0)
