080d766fd8f2aa8afc33ac49e8bdba7b347e36bc
czarjulius <juliusczar.jc@gmail.com>
2023-12-22 17:16:33 +0100

    rise21

--- leetcode/easy/mySqrt.js
+++ leetcode/easy/mySqrt.js
@@ -2,7 +2,7 @@ var mySqrt = function (x) {
   let lower = 0;
   let higher = x;
   while (lower <= higher) {
-    const mid = Math.floor((lower + higher) / 2);
+    // const mid = Math.floor((lower + higher) / 2);
     const square = mid * mid;
     if (square <= x) lower = mid + 1;
     else higher = mid - 1;
